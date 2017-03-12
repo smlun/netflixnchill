4ada48336135ce7014711188d5f18e22de320c4a
Ming Lun <smlun@msn.com>
2017-03-13 01:53:44 +0800

    Generate Orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,35 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @order = Order.new(movie_id: params[:movie_id])
+    stripe_token = params[:stripeToken]
+    payment_type = params[:stripeTokenType]
+    customer_email = params[:stripeEmail]
+    Stripe.api_key = "sk_test_L3iQjnqaaxHzHVBzXsgLmR7e"
+    Stripe::Charge.create(
+    amount: @movie.price*100,
+    currency: "usd",
+    source: stripe_token
+    )
+
+    @order.save
+      #send email
+      # redirect_to @movie, notice: 'Your order has been placed!'
+  end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+    def order_params
+      params.require(:order).permit(:movie_id)
+    end
+end
