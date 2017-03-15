ad3ad5dbaf56006c1b9a6db3df4466b24beb877e
Ming Lun <smlun@msn.com>
2017-03-15 10:03:54 +0800

    Implement validations for Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,6 @@
 class Movie < ApplicationRecord
   has_many :orders
 
-  validates :title, presence: true
-  validates :starring, presence: true
-  validates :price, presence: true
-  validates :description, presence: true 
+  validates :title, :starring, :price, :description, presence: true
+  validates :price, numericality: true
 end
