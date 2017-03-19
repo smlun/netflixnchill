ce6d1b00a8546545d1e05edc9d173a770bce3a1f
Ming Lun <smlun@msn.com>
2017-03-19 22:25:09 +0800

    Reconfigure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  get 'carts/index'
-
-  get 'carts/show'
-
-  get 'carts/new'
-
-  get 'carts/create'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies do
-    resources :orders
-  end
+  resources :movies
 
   resources :carts do
-    resources :orders
+    resources :line_items
   end
+
+  resources :orders
 
   root 'movies#disclaimer'
 end
